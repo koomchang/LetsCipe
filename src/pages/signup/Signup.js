a65d198eac695e94f867ccc51c2271cac104eb9f
koomchang <leahpar0401@gmail.com>
2023-05-05 17:37:52 +0900

    feat: signup page

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,7 +1,41 @@
+import { useState } from 'react'
 import styles from './Signup.module.css'
 
 export default function Signup() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [displayName, setDisplayName] = useState('');
+    const handleData = (event) => {
+        if (event.target.type === "email"){
+            setEmail(event.target.value);
+        } else if (event.target.type === "password"){
+            setPassword(event.target.value);
+        } else if (event.target.type === "text"){
+            setDisplayName(event.target.value);
+        }
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(email, password);
+    }
+
     return (
-        <div>Signup</div>
+        <form className={styles.signup_form} onSubmit={handleSubmit}>
+            <fieldset>
+                <legend>회원가입</legend>
+                <label htmlFor='myEmail'>email : </label>
+                <input type="email" id="myEmail" required value={email} onChange={handleData}/>
+
+                <label htmlFor='myPassWord'>password : </label>
+                <input type="password" id="myPassWord" required value={password} onChange={handleData}/>
+
+                <label htmlFor='myNickName'>NickName : </label>
+                <input type="text" id="myNickName" required value={email} onChange={handleData}/>
+
+                <button type='submit' className={styles.btn}>회원가입</button>
+            </fieldset>
+        </form>
     )
 }

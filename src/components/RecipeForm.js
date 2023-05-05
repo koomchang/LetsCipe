65138e6952da97fe08e7cafae1237860e50ee428
koomchang <leahpar0401@gmail.com>
2023-05-06 02:31:09 +0900

    add: RecipeForm component

---
+++ src/components/RecipeForm.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react"
+import { useFireStore } from "../hooks/useFireStore";
+
+export default function DiaryForm({ uid }) {
+
+    const [title, setTitle] = useState("");
+    const [text, setText] = useState("");
+    const { addDocument, response } = useFireStore('recipe');
+
+    const handleData = (event) => {
+        if (event.target.id === 'tit') {
+            setTitle(event.target.value);
+        } else if (event.target.id === "txt") {
+            setText(event.target.value);
+        }
+    }
+
+    useEffect(() => {
+        if(response.success){
+            setTitle('');
+            setText('');
+        }
+    }, [response.success])
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(title, text);
+        addDocument({ uid, title, text })
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <fieldset>
+                    <legend>레시피 작성</legend>
+                    <label htmlFor="tit">제목 : </label>
+                    <input id="tit" type="text" value={title} required onChange={handleData} />
+
+                    <label htmlFor="txt">내용 : </label>
+                    <textarea id="txt" type="text" value={text} required onChange={handleData}></textarea>
+
+                    <button type="submit">저장하기</button>
+                </fieldset>
+            </form>
+        </>
+    )
+}

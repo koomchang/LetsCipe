f5621f03a4a47b7a17116551b1cefc973f6b9b77
koomchang <leahpar0401@gmail.com>
2023-05-06 02:31:32 +0900

    feat: 홈 화면

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,19 @@
 import styles from './Home.module.css'
+import RecipeForm from '../../components/RecipeForm'
+import { useAuthContext } from '../../hooks/useAuthContext'
 
 export default function Home() {
+
+    const { user } = useAuthContext();
+
     return (
-        <div>Home</div>
+        <main className={styles.cont}>
+            <aside className={styles.side_menu}>
+                <RecipeForm uid={user.uid}></RecipeForm>
+            </aside>
+            <ul className={styles.content_list}>
+                Recipe list
+            </ul>
+        </main>
     )
 }

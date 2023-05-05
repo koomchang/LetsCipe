d6e5079639f2da2134fe2dcb4e9c97127a924ab8
koomchang <leahpar0401@gmail.com>
2023-05-05 19:19:01 +0900

    feat: 로그인 기능 구현

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,16 @@
 import { Link } from 'react-router-dom'
 import styles from './Nav.module.css'
+import { useLogout } from '../hooks/useLogout'
 
-export default function Nav(){
+export default function Nav() {
+    const { logout } = useLogout();
     return (
         <nav className={styles.nav}>
             <h1 className={styles.tit}>Let's Cipe</h1>
             <ul className={styles.list_nav}>
                 <li><Link to="/login">로그인</Link></li>
                 <li><Link to="/signup">회원가입</Link></li>
+                <li><button type="button" onClick={logout}>로그아웃</button></li>
             </ul>
         </nav>
     )
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -6,6 +6,8 @@ const authReducer = (state, action) => {
     switch (action.type) {
         case 'login':
             return { ...state, user: action.payload }
+        case 'logout':
+            return { ...state, user: null }
         default:
             return state
     }
@@ -16,6 +18,8 @@ const AuthContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(authReducer, {
         user: null
     })
+
+    console.log('user state : ', state)
 
     return (
         <AuthContext.Provider value={{ ...state, dispatch }}>

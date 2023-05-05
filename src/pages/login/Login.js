88ee35dd6fdb980033c7c15ba9d1b3722dea37a3
koomchang <leahpar0401@gmail.com>
2023-05-05 19:18:38 +0900

    feat : 로그인 통신 상태에 따른 메세지 관리

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,21 +1,24 @@
 import { useState } from 'react'
 import styles from './Login.module.css'
+import { useLogin } from '../../hooks/useLogin';
 
 export default function Login() {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const { error, isPending, login } = useLogin();
+
     const handleData = (event) => {
-        if (event.target.type === "email"){
+        if (event.target.type === "email") {
             setEmail(event.target.value);
-        } else if (event.target.type === "password"){
+        } else if (event.target.type === "password") {
             setPassword(event.target.value);
         }
     }
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(email, password);
+        login(email, password);
     }
 
     return (
@@ -23,12 +26,15 @@ export default function Login() {
             <fieldset>
                 <legend>로그인</legend>
                 <label htmlFor='myEmail'>email : </label>
-                <input type="email" id="myEmail" required value={email} onChange={handleData}/>
+                <input type="email" id="myEmail" required value={email} onChange={handleData} />
 
                 <label htmlFor='myPassWord'>password : </label>
-                <input type="password" id="myPassWord" required value={password} onChange={handleData}/>
+                <input type="password" id="myPassWord" required value={password} onChange={handleData} />
 
-                <button type='submit' className={styles.btn}>로그인</button>
+                {!isPending && <button type='submit' className={styles.btn}>로그인</button>}
+                {isPending && <strong>로그인 진행중입니다..!</strong>}
+                {error && <strong>{error}</strong>}
+
             </fieldset>
         </form>
     )

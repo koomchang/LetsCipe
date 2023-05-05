56c39e0731653eb674bd62ec4005ce1504271bf4
koomchang <leahpar0401@gmail.com>
2023-05-05 18:47:52 +0900

    feat: 회원가입 hook 생성, context로 유저 정보 관리

---
+++ src/hooks/useAuthContext.js
@@ -0,0 +1,8 @@
+import { useContext, usecontext } from "react";
+import { AuthContext } from "../context/AuthContext";
+
+export const useAuthContext = () => {
+    const context = useContext(AuthContext);
+
+    return context;
+}
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,38 +1,41 @@
 import { useState } from 'react'
 import styles from './Signup.module.css'
+import { useSignup } from '../../hooks/useSignup';
 
 export default function Signup() {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [displayName, setDisplayName] = useState('');
+    const { error, isPending, signup } = useSignup();
+
     const handleData = (event) => {
-        if (event.target.type === "email"){
+        if (event.target.type === "email") {
             setEmail(event.target.value);
-        } else if (event.target.type === "password"){
+        } else if (event.target.type === "password") {
             setPassword(event.target.value);
-        } else if (event.target.type === "text"){
+        } else if (event.target.type === "text") {
             setDisplayName(event.target.value);
         }
     }
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(email, password);
+        signup(email, password, displayName);
     }
 
     return (
         <form className={styles.signup_form} onSubmit={handleSubmit}>
             <fieldset>
                 <legend>회원가입</legend>
-                <label htmlFor='myEmail'>email : </label>
-                <input type="email" id="myEmail" required value={email} onChange={handleData}/>
+                <label htmlFor='myEmail'>이메일 : </label>
+                <input type="email" id="myEmail" required value={email} onChange={handleData} />
 
-                <label htmlFor='myPassWord'>password : </label>
-                <input type="password" id="myPassWord" required value={password} onChange={handleData}/>
+                <label htmlFor='myPassWord'>비밀번호 : </label>
+                <input type="password" id="myPassWord" required value={password} onChange={handleData} />
 
-                <label htmlFor='myNickName'>NickName : </label>
-                <input type="text" id="myNickName" required value={email} onChange={handleData}/>
+                <label htmlFor='myNickName'>닉네임 : </label>
+                <input type="text" id="myNickName" required value={displayName} onChange={handleData} />
 
                 <button type='submit' className={styles.btn}>회원가입</button>
             </fieldset>

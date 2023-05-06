4c144a78512c3dc3f62c0e4ef1b31928c02c0b94
koomchang <leahpar0401@gmail.com>
2023-05-07 03:27:09 +0900

    add: logo 사진 넣기

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,7 +8,7 @@ export default function Nav() {
     const { user } = useAuthContext();
     return (
         <nav className={styles.nav}>
-            <h1 className={styles.tit}>Let's Cipe</h1>
+            <img className={styles.img} src='assets/logo.png' />
             <ul className={styles.list_nav}>
                 {!user &&
                     <>
@@ -17,13 +17,13 @@ export default function Nav() {
                     </>
                 }
                 {user &&
-                    <li>
+                    <>
                         <strong>환영합니다 {user.displayName}님!</strong>
-                        <li><Link to="/new-recipe">레시피 작성하기</Link></li>
-                        <li><Link to="/my-recipe">나의 레시피</Link></li>
                         <li><Link to="/recipes">모든 레시피</Link></li>
+                        <li><Link to="/my-recipe">나의 레시피</Link></li>
+                        <li><Link to="/new-recipe">레시피 작성하기</Link></li>
                         <button type="button" onClick={logout}>로그아웃</button>
-                    </li>
+                    </>
                 }
             </ul>
         </nav>

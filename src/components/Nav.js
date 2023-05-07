a21131f2af44eaa231083a11c7eb452ace72e168
koomchang <leahpar0401@gmail.com>
2023-05-07 21:46:30 +0900

    fix: navbar css

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,19 +11,24 @@ export default function Nav() {
             <Link to="/">
                 <img className={styles.img} src='assets/logo.png' />
             </Link>
+            {user &&
+                <>
+                    <p>환영합니다!&nbsp;&nbsp; {user.displayName} 님!</p>
+
+                </>
+            }
             <ul className={styles.list_nav}>
                 {!user &&
                     <>
-                        <li><Link to="/login">로그인</Link></li>
-                        <li><Link to="/signup">회원가입</Link></li>
+                        <li><Link className={styles.link} to="/login">로그인</Link></li>
+                        <li><Link className={styles.link} to="/signup">회원가입</Link></li>
                     </>
                 }
                 {user &&
                     <>
-                        <strong>환영합니다 {user.displayName}님!</strong>
-                        <li><Link to="/recipes">모든 레시피</Link></li>
-                        <li><Link to="/my-recipe">나의 레시피</Link></li>
-                        <li><Link to="/new-recipe">레시피 작성하기</Link></li>
+                        <li><Link className={styles.link} to="/recipes">모든 레시피</Link></li>
+                        <li><Link className={styles.link} to="/my-recipe">나의 레시피</Link></li>
+                        <li><Link className={styles.link} to="/new-recipe">레시피 작성하기</Link></li>
                         <button className={styles.logoutBtn} type="button" onClick={logout}>로그아웃</button>
                     </>
                 }
